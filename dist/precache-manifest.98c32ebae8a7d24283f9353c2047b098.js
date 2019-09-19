a2505940e5dfbb8e857de83363aebfcb60eb124c
wddbg-Fei <751821957@qq.com>
2019-09-19 14:48:38 +0800

    打包提交

---
+++ dist/precache-manifest.98c32ebae8a7d24283f9353c2047b098.js
@@ -0,0 +1,146 @@
+self.__precacheManifest = [
+  {
+    "revision": "c82b480aca4215edaa9f",
+    "url": "css/Msite.c59a2ea9.css"
+  },
+  {
+    "revision": "0a101c460f87fd10c5e9fd889ee5fc1d",
+    "url": "index.html"
+  },
+  {
+    "revision": "de81818b3dca64bce6e1",
+    "url": "js/chunk-vendors.33284d2a.js"
+  },
+  {
+    "revision": "f4753e56d93c44b5b37f",
+    "url": "js/AddDetail.d8d191c7.js"
+  },
+  {
+    "revision": "68c029f5fcb0d2a4e74d",
+    "url": "js/app.c6054d55.js"
+  },
+  {
+    "revision": "053893ce4bcd9af83d12",
+    "url": "js/Address.6db9dc32.js"
+  },
+  {
+    "revision": "864a4ddf0f8a0b61a49f",
+    "url": "js/Shop.3d16e15a.js"
+  },
+  {
+    "revision": "e85d473c94ab1bb6ca07",
+    "url": "js/City.af366c37.js"
+  },
+  {
+    "revision": "f4f56804590858e37d17",
+    "url": "js/Seek.24231ce5.js"
+  },
+  {
+    "revision": "4530da3e78c36f158ea0",
+    "url": "js/Home.1434dbad.js"
+  },
+  {
+    "revision": "777bd4dafd5faea16dd6",
+    "url": "js/Order.45c7bccd.js"
+  },
+  {
+    "revision": "2e0fdfac2472ee4f9d51",
+    "url": "js/Indent.efdc6d72.js"
+  },
+  {
+    "revision": "c82b480aca4215edaa9f",
+    "url": "js/Msite.f29e9cf5.js"
+  },
+  {
+    "revision": "f36e0841630703afcb16",
+    "url": "js/Info.4d34837f.js"
+  },
+  {
+    "revision": "e13533df06ff2f48a90c",
+    "url": "js/Add.6ed5b013.js"
+  },
+  {
+    "revision": "39fd63dfec5875a86593",
+    "url": "js/Login.30b70526.js"
+  },
+  {
+    "revision": "5b7f536daa0b57f2b343",
+    "url": "js/Mine.e122f7ef.js"
+  },
+  {
+    "revision": "68c029f5fcb0d2a4e74d",
+    "url": "css/app.545a09d5.css"
+  },
+  {
+    "revision": "5b7f536daa0b57f2b343",
+    "url": "css/Mine.715d14da.css"
+  },
+  {
+    "revision": "344a5e709f1ddbb807656547e9a39b2e",
+    "url": "img/1.344a5e70.png"
+  },
+  {
+    "revision": "777bd4dafd5faea16dd6",
+    "url": "css/Order.0d7bb89a.css"
+  },
+  {
+    "revision": "20cac1158f04a73a2d198f2e4973f51b",
+    "url": "img/1.20cac115.gif"
+  },
+  {
+    "revision": "f4f56804590858e37d17",
+    "url": "css/Seek.dc404509.css"
+  },
+  {
+    "revision": "fd82fca8f2371dac2cb4b1e4d759072e",
+    "url": "img/1.fd82fca8.png"
+  },
+  {
+    "revision": "864a4ddf0f8a0b61a49f",
+    "url": "css/Shop.c5960f5e.css"
+  },
+  {
+    "revision": "732389ded34cb9c52dd88271f1345af9",
+    "url": "fonts/element-icons.732389de.ttf"
+  },
+  {
+    "revision": "535877f50039c0cb49a6196a5b7517cd",
+    "url": "fonts/element-icons.535877f5.woff"
+  },
+  {
+    "revision": "de81818b3dca64bce6e1",
+    "url": "css/chunk-vendors.c6f43817.css"
+  },
+  {
+    "revision": "39fd63dfec5875a86593",
+    "url": "css/Login.b6b78e12.css"
+  },
+  {
+    "revision": "f36e0841630703afcb16",
+    "url": "css/Info.a1924778.css"
+  },
+  {
+    "revision": "2e0fdfac2472ee4f9d51",
+    "url": "css/Indent.4bcf800a.css"
+  },
+  {
+    "revision": "4530da3e78c36f158ea0",
+    "url": "css/Home.1b789640.css"
+  },
+  {
+    "revision": "e85d473c94ab1bb6ca07",
+    "url": "css/City.7652dff9.css"
+  },
+  {
+    "revision": "053893ce4bcd9af83d12",
+    "url": "css/Address.b28c2f98.css"
+  },
+  {
+    "revision": "f4753e56d93c44b5b37f",
+    "url": "css/AddDetail.e74a2373.css"
+  },
+  {
+    "revision": "e13533df06ff2f48a90c",
+    "url": "css/Add.1e76544e.css"
+  }
+];
